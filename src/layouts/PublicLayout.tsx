b8f036035d115090f85b9a0e49b4c09433e7e1d0
rishabh <rishabhguptalahar@gmail.com>
2025-05-04 10:59:25 +0530

    setup the firebase database to save the user data

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Footer"
 import {Header} from "@/components/Header"
+import AuthHandler from "@/handlers/AuthHandler"
 import { Outlet } from "react-router-dom"
 
 function PublicLayout() {
   return (
     <div className="w-full">
         {/* handler to store the user data */}
+        <AuthHandler />
         <Header />
 
         <Outlet />
